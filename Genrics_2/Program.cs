fd760a5396edb7286a7b7fe571bf830c5bb2cc19
Sudhir <Sudhirkaul1@gmail.com>
2023-07-07 17:24:37 +1200

    more generics

--- Genrics_2/Program.cs
+++ Genrics_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
 
 namespace Generics2;
 
@@ -22,6 +23,26 @@ static class EnumerableExtensions
         }
     }
 
+    public static int MyCount<T>(this IEnumerable<T> that)
+    {
+        var collections = that as ICollection<T>;
+        if (collections != null)
+            return collections.Count;
+
+        return 0;
+    }
+
+    public static int MyCount<T>(this IEnumerable<T> that , Func<T,bool> predicate)
+    {
+        var cnt = 0;
+        foreach(var item in that)
+        {
+            if (predicate(item))
+                cnt++;
+        }
+        return cnt;
+    }
+
     //public static IEnumerable<TResult> MyAny<TSource,TResult>(this IEnumerable<TSource> that, Func<TSource,bool> predicate)
     //{
     //    foreach(var item in that)
@@ -32,10 +53,17 @@ static class EnumerableExtensions
 
     public static T MyFirst<T>(this IEnumerable<T> that)
     {
-        foreach (var item in that)
-           return item;
+        using (var enumerable = that.GetEnumerator())
+        { 
+            if (!enumerable.MoveNext())
+                throw new InvalidOperationException();
+
+            return enumerable.Current;
+        }
+        //foreach (var item in that)
+        //   return item;
         
-        throw new InvalidOperationException();
+        //throw new InvalidOperationException();
     }
 
     public static T MyFirst<T>(this IEnumerable<T> that, Func<T, bool> predicate)
@@ -56,7 +84,7 @@ class Generics2
     static void Main(string[] args)
     {
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        //IEnumerable<int> filteredList = numbers.Where(i => i > 4);
+        //IEnumerable<int> filteredList = numbers.Where(i => i > 4);    
         IEnumerable<int> filteredList = numbers.MyWhere(i => i > 4);
         //IEnumerable<int> squared = filteredList.Select(i => i * i);
         IEnumerable<int> squared = filteredList.MySelect(i => i * i);
@@ -81,6 +109,8 @@ class Generics2
         int firstFunc = squared.MyFirst(i => i > 50);
 
         Console.WriteLine(firstFunc);
+
+        Console.Write(squared.MyCount(i => i > 50));
 
     }
 
